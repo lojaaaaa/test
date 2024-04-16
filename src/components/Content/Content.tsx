ced8f54a55ad30c7f49172351914c345ebcf4b67
Аюгов Александр <a.ayugov@zebrains.team>
2024-04-16 09:41:51 +0400

    fdff

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import Select from 'react-select';
+import style from './Content.module.scss'
+
+const Content = () => {
+
+  const [selectValue, setSelectValue] = useState(null);
+
+  // const handleChangeSelect = (value) => {
+  //   setSelectValue(value)
+  // }
+
+  // const selectedLanguage = selectValue && 
+  //   languagesDetails.find(el => el.value === selectValue.value)
+
+  return (
+    <div className={style.content}>
+      <h1 className={style.title}>
+        Here you can get information about some programming languages
+      </h1>
+
+      <p>fdf</p>
+      {/* <div className={style.info}>
+        <Select
+          placeholder="Choose a programming language"
+          name="color"
+          options={languagesOptions}
+          isSearchable={false}
+          value={selectValue}
+          onChange={handleChangeSelect}
+        />
+        <div className={style.image}>
+          <img src="./1.png" alt="empty" />
+        </div>
+        { 
+          selectedLanguage &&
+          <Info selectedLanguage={selectedLanguage}/>
+        }
+      </div> */}
+    </div>
+  )
+}
+
+export default Content
